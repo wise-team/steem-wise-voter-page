fa1abb11ec164805bd1d2410da1f0373e7fdc43c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-15 23:00:12 +0200

    Add spinner to RuleSelectorForm

--- src/components/form/RuleSelectorForm.vue
+++ src/components/form/RuleSelectorForm.vue
@@ -6,10 +6,19 @@
                 &nbsp; Step 2: Select a ruleset
             </h4>
             <b-container fluid>
-                <b-row><b-col class="text-center">
-                    <button class="btn btn-primary" :disabled="isButtonDisabled">
+                <b-row><b-col><p class="text-center">
+                    <button :class="[isButtonDisabled? 'btn btn-lg btn-secondary' : 'btn btn-lg btn-primary']" :disabled="isButtonDisabled" @click="loadButtonClick">
                         Load rules for @{{ voterUsername }} from @{{ delegatorUsername }}
                     </button>
+                </p></b-col></b-row>
+                <b-row><b-col>
+                    <p>
+                        <b-alert variant="info" dismissible :show="loadingMessage.length > 0">{{ loadingMessage }}</b-alert>
+                        <b-alert variant="danger" dismissible :show="loadingError.length > 0">{{ loadingError }}</b-alert>
+                    </p>
+                    <p v-if="loadingInProggress" class="text-center text-muted loading-icon">
+                        <font-awesome-icon :icon="loadingIcon" spin />
+                    </p>
                 </b-col></b-row>
             </b-container>
         </div>
@@ -20,6 +29,7 @@ import Vue from "vue";
 import { mapGetters } from 'vuex';
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 import faArrowCircleRight from '@fortawesome/fontawesome-free-solid/faArrowCircleRight'
+import faCog from '@fortawesome/fontawesome-free-solid/faCog'
 
 export default Vue.extend({
     props: [],
@@ -28,16 +38,26 @@ export default Vue.extend({
         }
     },
     methods: {
-
+        loadButtonClick(): void {
+            if(this.$store.getters.rulesSelectorFormEnabled && !this.$store.state.rulesetLoadingState.inProggress) {
+                this.$store.dispatch('loadRulesets');
+            }
+        }
     },
     computed: {
         arrowRightIcon () { return faArrowCircleRight; },
+        loadingIcon () { return faCog; },
         ...mapGetters({ 
             isEnabled: 'rulesSelectorFormEnabled'
         }),
-        isButtonDisabled (): boolean { return !this.$store.getters.rulesSelectorFormEnabled; }, // disable when loading data from blockchain
+        isButtonDisabled (): boolean { 
+            return ! (this.$store.getters.rulesSelectorFormEnabled && !this.$store.state.rulesetLoadingState.inProggress); 
+        },
         delegatorUsername (): string { return this.$store.state.delegatorUsername; },
-        voterUsername (): string { return this.$store.state.voterUsername; }
+        voterUsername (): string { return this.$store.state.voterUsername; },
+        loadingInProggress(): boolean { return this.$store.state.rulesetLoadingState.inProggress },
+        loadingMessage(): string { return this.$store.state.rulesetLoadingState.message },
+        loadingError(): string { return this.$store.state.rulesetLoadingState.error }
     },
     components: {
         FontAwesomeIcon
@@ -46,5 +66,7 @@ export default Vue.extend({
 </script>
 
 <style>
-
+    .loading-icon {
+        font-size: 4rem;
+    }
 </style>
