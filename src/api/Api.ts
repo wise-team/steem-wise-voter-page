14937366f60099c2a9fdbd443f964f7902113853
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-06-04 21:55:59 +0200

    Move Api to new Wise

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,5 +1,6 @@
 import { Promise } from "bluebird";
 import * as steem from "steem";
+import { Wise, SetRules, DirectBlockchainApi, SendVoteorder, ValidationException } from "steem-wise-core";
 
 export class Api {
     public static validateAccountsExistence(delegator: string, voter: string): Promise<void> {
@@ -18,60 +19,28 @@ export class Api {
         });
     }
 
-    public static loadRulesets(delegator: string, voter: string): (() => Promise<smartvotes_ruleset []>) {
-        return () => {
-            return new Promise((resolve, reject) => {
-                const smartvotes = new SteemSmartvotes("username-not-required", "wif-not-requiored");
-                smartvotes.getRulesetsOfUser(delegator,
-                        (error: Error | undefined, rulesets: smartvotes_ruleset []): void => {
-                    if (error) reject(error);
-                    else {
-                        if (rulesets.length === 0) reject(new Error("Delegator (@" + delegator + ") has no rulesets."));
-                        else {
-                            const rulesetsForVoter = [];
-                            for (let i = 0; i < rulesets.length; i ++) {
-                                if (rulesets[i].voter === voter) {
-                                    rulesetsForVoter.push(rulesets[i]);
-                                }
-                            }
-                            if (rulesetsForVoter.length === 0) reject(new Error(
-                                "Delegator (@" + delegator + ") has no rulesets for this voter (@" + voter + ")."));
-                            else resolve(rulesetsForVoter);
-                        }
-                    }
-                });
-            });
-        };
+    public static loadRulesets(delegator: string, voter: string): (() => Promise<SetRules>) {
+        const voterWise = new Wise(voter, new DirectBlockchainApi(voter, "wif-not-necessary"));
+        return () => voterWise.getRulesetsAsync(delegator);
+
     }
 
-    public static validateVoteorder(voter: string, voteorder: smartvotes_voteorder,
+    public static validateVoteorder(delegator: string, voter: string, voteorder: SendVoteorder,
                                     proggressCallback: (msg: string, proggress: number) => void):
                                     Promise<void> {
-        return new Promise((resolve, reject) => {
-            const smartvotes = new SteemSmartvotes("username-not-required", "wif-not-requiored");
-            smartvotes.validateVoteOrder(voter, voteorder, SteemOperationNumber.FUTURE,
-                (error: Error | undefined, result: boolean): void => {
-                    if (error) reject(error);
-                    else if (result) {
-                        resolve();
-                    } else reject(new Error("Inconsistent state: invalid, but no error thrown"));
-            }, proggressCallback);
+        const voterWise = new Wise(voter, new DirectBlockchainApi(voter, "wif-not-necessary"));
+        return voterWise.validatePotentialVoteorderAsync(delegator, voter, voteorder, proggressCallback)
+        .then((result: true | ValidationException) => {
+            if (result !== true) throw result;
         });
     }
 
-    public static sendVoteorder(voter: string, postingWif: string, voteorder: smartvotes_voteorder,
+    public static sendVoteorder(voter: string, postingWif: string, delegator: string, voteorder: SendVoteorder,
                                 proggressCallback: (msg: string, proggress: number) => void):
                                     Promise<void> {
-        return new Promise((resolve, reject) => {
-            const smartvotes = new SteemSmartvotes(voter, postingWif);
-            smartvotes.sendVoteOrder(voteorder,
-                (error: Error | undefined, result: boolean): void => {
-                if (error) reject(error);
-                else if (result) {
-                    resolve();
-                } else reject(new Error("Inconsistent state: invalid, but no error thrown"));
-            }, proggressCallback);
-        });
+        const voterWise = new Wise(voter, new DirectBlockchainApi(voter, postingWif));
+        return voterWise.sendVoteorderAsync(delegator, voteorder, proggressCallback)
+        .then(() => { /**/ });
     }
 
     public static isWif(key: string): boolean {
