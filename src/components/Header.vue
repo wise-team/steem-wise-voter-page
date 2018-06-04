96a6a31ede98f30a40420a4d35315ccbb0fef990
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-06-04 22:30:09 +0200

    Update header & footer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,8 @@
 <template>
         <div class="text-center header">
             <img class="d-block mx-auto mb-4" src="assets/steem-logo.svg" alt="" width="100" height="100">
-            <h1 class="text-muted">steem<wbr /> smart<wbr />votes</h1>
-            <p class="lead">A smart tool that allows you to easily vote on behalf of your delegator. Put your posting key,
+            <h1 class="text-muted">steem<wbr /> WISE</h1>
+            <p class="lead">A wise tool that allows you to easily vote on behalf of your delegator. Put your posting key,
                 type a delegator's username and paste steemit link. Voila! </p>
         </div>
 </template>
