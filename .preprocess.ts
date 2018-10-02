416c970e1a0e39eab10b7d9776c1d9d393802595
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-10-02 20:13:06 +0200

    ci: add preprocess script

---
+++ .preprocess.ts
@@ -0,0 +1,9 @@
+// this file is loaded by wise-ci preprocessor
+
+export const hooks = [];
+export const data = {
+    repository: {
+        name: "steem-wise-voter-page"
+    }
+};
+export const excludes = [ "dist", ".vscode", "coverage" ];
